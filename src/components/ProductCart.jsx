a361a40066714ca84a2fa0619de42256e6cf6bed
chattq <baotuyet927@gmail.com>
2023-04-03 09:30:43 +0700

    update

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -34,7 +34,6 @@ export default function ProductCart({ data }) {
     );
   };
   const handleMinusNumber = (data) => {
-    console.log(37, data.quantity);
     dispatch(
       addToCart({
         ...data,
@@ -46,7 +45,6 @@ export default function ProductCart({ data }) {
       dispatch(removeItem({ color: data?.color }));
     }
   };
-  console.log(49, data.quantity);
   return (
     <div className="bg-[rgba(0,0,0,.03)] rounded-[6px]">
       <div className="flex p-[15px] items-center border-b-[1px]">
